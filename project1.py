7822d5f5603b5e1b80834aa46bed5e4a57f9da15
a-mhnds <alimohandesi@gmail.com>
2025-05-16 15:54:28 -0600

    updating the project1.py file to its final format and adding the tasks.jason file in .vscode folder

--- project1.py
+++ project1.py
@@ -1,1 +1,30 @@
 import random
+
+play_choice = {1:'rock',2:'paper', 3:'scissors'}
+val = list(play_choice.values())
+computer_play = random.choice(val)
+
+human_play = input('rock, paper, or scissors?')
+out_str = f'''I chose {computer_play}, and you chose {human_play}.'''
+print(out_str)
+if human_play in play_choice.values():
+    if human_play == computer_play:
+        print('Draw!')
+    else:
+        if human_play == 'rock':
+            if computer_play == 'scissors':
+                print('You won!')
+            else:
+                print('I won!')
+        if human_play == 'paper':
+            if computer_play == 'scissors':
+                print('I won!')
+            else:
+                print('you won!')
+        if human_play == 'scissors':
+            if computer_play == 'rock':
+                print('I won!')
+            else:
+                print('you won!')
+else:
+    print('Error: Incorrect choice!')
